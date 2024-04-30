e101173468b63f5a667e31b05d68fbd77b560d2b
Muhammad Humza Pirzada <112612741+Muhammad-Humza-Pirzada@users.noreply.github.com>
2024-04-30 14:40:04 +0500

    Add files via upload

---
+++ dom-js.js
@@ -0,0 +1,14 @@
+
+// document.getElementById("heading1").innerHTML = "hello saad";
+// document.getElementById("heading1").style.backgroundColor = "red";
+// document.getElementById('heading1').style.color = "white";
+
+function changeText(){
+    document.getElementById("heading1").innerHTML = "There was clicked";
+}
+
+function changeTheme(){
+    document.getElementById("heading1").style.backgroundColor = "black";
+    document.getElementById("heading1").style.color = "white";
+    
+}
